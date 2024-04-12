a37c65544c81d8e3012171d16e7d95e05e84e705
Garrett Gee <garrett.m.gee@gmail.com>
2024-04-12 17:01:37 -0400

    added next month days and next month state to maintain conistant grid

--- components/calendar/calendar.js
+++ components/calendar/calendar.js
@@ -15,6 +15,7 @@ export default function Calendar() {
   prevDateObj.setMonth(currentDate.getMonth() - 1);
   const [date, setDate] = useState(currentDate);
   const [prevDate, setPrevDate] = useState(prevDateObj);
+  const [nextDate, setNextDate] = useState(prevDateObj);
   const [showModal, setShowModal] = useState(false);
   const [selectedCell, setSelectedCell] = useState(0);
   const [darkMode, setDarkMode] = useState(false);
@@ -35,6 +36,13 @@ export default function Calendar() {
   
   // Function to get the number of days in the previous month
   const getPrevMonthDays = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    return new Date(year, month, 0).getDate();
+  }
+
+  // Function to get the number of days in the previous month
+  const getNextMonthDays = (date) => {
     const year = date.getFullYear();
     const month = date.getMonth();
     return new Date(year, month, 0).getDate();
