44320904ca01872ef59bbbc86595318e5e3d9208
Garrett Gee <garrett.m.gee@gmail.com>
2024-04-18 00:41:23 -0400

    added tabular icon for delete item

--- components/event/event.js
+++ components/event/event.js
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import mongoose from 'mongoose';
 import { useEffect } from 'react';
+import { IconTrash } from '@tabler/icons-react';
 // import Navigation from '../navigation/navigation';
 // import Modal from '../modal/modal';
 import { model, connectionURI } from '../mongo/db.mjs';
@@ -12,6 +13,7 @@ export default function Event() {
     return (
         <div className="event">
             <input type="text" className="event-input" placeholder="Enter event here..."></input>
+            {<IconTrash color="white" size="20"/>}
         </div>
     );  
 };
