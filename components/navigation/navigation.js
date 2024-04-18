eb9dc0bfef0c47ccf05727c33c8603d381bbcc9e
Garrett Gee <garrett.m.gee@gmail.com>
2024-04-18 02:41:00 -0400

    replace html codes with tabler icons

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,11 +1,17 @@
 "use client";
+import { IconChevronLeft } from '@tabler/icons-react';
+
 
 export default function Navigation({currentDate, prevMonth, nextMonth, mode}) {
     return(
         <div id="navigation" className="flex header">
-            <button className="arrow-button" onClick={prevMonth}>&lt;</button>
+            <button className="arrow-button" onClick={prevMonth}>
+                <IconChevronLeft size={20} color="white"/>
+            </button>
             <span className="grow text-center">{currentDate.toLocaleString('default', { month: 'long', year: 'numeric' })}{mode!=="normal"?`(${mode})`:""}</span>
-            <button className="arrow-button" onClick={nextMonth}>&gt;</button>
+            <button className="arrow-button" onClick={nextMonth}>
+            <IconChevronRight size={20} color="white"/>
+            </button>
         </div>
     );
 
